8784d8beeeb86d3d34c844cb817948115de21c9c
askeugjoh <61004362+askeugjoh@users.noreply.github.com>
2022-07-22 08:01:41 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,4 @@
+import tkinter
+win=tkinter.Tk() #creating the main window and storing the window object in 'win'
+#We create the widgets here
+win.mainloop() #running the loop that works as a trigger
